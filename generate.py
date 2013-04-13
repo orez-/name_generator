3d1f8cbf86cbfa170a0ee3b144627f08b3a7f41b
orez- <bshaginaw120@gmail.com>
2013-04-13 14:18:46 -0400

    Added minimum length and lookbehind, although lookbehind doesn't really add much.

--- generate.py
+++ generate.py
@@ -3,7 +3,9 @@ import re
 from string import uppercase
 import sys
 
+MINLEN = 3
 LOOKAHEAD = 3
+LOOKBEHIND = 1
 r = re.compile("([A-Z]+)\s*(\d)\.(\d{3})")
 names = []
 with open("androg.txt", "r") as f:
@@ -13,32 +15,34 @@ with open("androg.txt", "r") as f:
             name, percint, percfrac = m.groups()
             names.append((name, int(percint) * 1000 + int(percfrac)))
 
-letters = list(uppercase) + [None]
+letters = [None]
 letter_list = {letter: {"total": 0} for letter in letters}
 # one letter lookbehind & lookahead, for now
 
 for name, perc in names:  # generate table
     letter_list[None][name[0]] = letter_list[None].get(name[0], 0) + perc
     letter_list[None]["total"] += perc
-    for i, letter in enumerate(name):
-        tperc = perc // name.count(letter)
+    for i, _ in enumerate(name):
+        letter = name[max(0, i - LOOKBEHIND + 1): i + 1].rjust(LOOKBEHIND, "-")
+        tperc = perc  # // name.count(letter)
         next = name[i + 1: i + 1 + LOOKAHEAD].ljust(LOOKAHEAD, "-")
+        letter_list.setdefault(letter, {"total": 0})
         letter_list[letter][next] = letter_list[letter].get(next, 0) + tperc
         letter_list[letter]["total"] += perc
 
 my_name = ""
 last_letter = None
-while last_letter != '-':
+while not last_letter or last_letter[-1] != '-':
     ws = letter_list[last_letter]
     num = random.randint(0, ws["total"])
     for letter, value in ws.iteritems():
         if letter == "total":
             continue
         num -= value
-        if num < 0:
+        if num < 0 and not (letter[-1] == "-" and len((my_name + letter).rstrip("-")) < MINLEN):
             my_name += letter
-            last_letter = my_name[-1]
+            last_letter = my_name[-LOOKBEHIND:].rjust(LOOKBEHIND, "-")
             break
 
-my_name = my_name.rstrip("-")
+my_name = my_name.rstrip("-").title()
 print my_name
